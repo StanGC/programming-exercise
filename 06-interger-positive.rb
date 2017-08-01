dfbad6d681611f1d8e54aaf4e7a45811286d8f29
Stan <solve153@gmail.com>
2017-08-01 23:49:11 +0800

    06-interger-positive.rb ok

--- 06-interger-positive.rb
+++ 06-interger-positive.rb
@@ -2,9 +2,20 @@
 
 
 print "请输入一个整数，然后按 Enter: "
-x = gets
+x = gets().to_i
 
 # ....
 
-puts "这个数是_____ (正数或零或负数)"
-puts "这个数是_____ (偶数或奇数)"
+if x > 0
+  result_1 = "正數"
+elsif x == 0
+  result_1 = "零"
+else
+  result_1 = "負數"
+end
+
+result_2 = (x % 2 == 0) ? "偶數" : "奇數"
+
+puts "你輸入了 #{x}"
+puts "这个数是 #{result_1}"
+puts "这个数是 #{result_2}"
